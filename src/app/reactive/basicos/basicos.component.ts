6bf467dd698b3a5904f2ca5ba8c5cdb7911008ab
pbl.gllgs <pbl.gllgs@gmail.com>
2021-10-18 13:33:28 -0300

    updates 18-10-2021

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,6 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  miFormulario : FormGroup = this.fb.group({
+    nombre :[,[Validators.required,Validators.minLength(3)]],
+    precio :[,[Validators.required,Validators.min(0)]],
+    existencias :[,[Validators.required,Validators.min(0)]]
+  })
+
+  constructor(private fb :FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre:'RTX 4080TI',
+      precio: 2599,
+      existencias: 10
+    });
   }
+
+  campoEsValido(campo:string){
+
+    return  this.miFormulario.controls[campo].errors 
+            && this.miFormulario.controls[campo].touched;
+  }
+
+  guardar(){
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    this.miFormulario.reset();
+  }
+
 
 }
--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  public nuevoJuego:string = '';
 
-  ngOnInit(): void {
+  miFormulario : FormGroup = this.fb.group({
+    nombre :[,[Validators.required,Validators.minLength(3)]],
+    favoritos: this.fb.array( [
+      ['Dying ligth',Validators.required],
+      ['Cyberpunk 2077',Validators.required]
+    ], Validators.required )
+  })
+
+  constructor(private fb:FormBuilder) { }
+
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray;
   }
+
+
+  campoEsValido(campo:string){
+    return  this.miFormulario.controls[campo].errors 
+            && this.miFormulario.controls[campo].touched;
+  }
+
+
+  guardar(){
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value)
+    this.miFormulario.reset();
+  }
+
+  agregarJuego(){
+    
+  }
+
+  eliminar(index :number){
+    
+  }
+
 
 }
